4917147116e69aee424f8332818d879c2b9a775f
Peter Csajtai <peter.csajtai@outlook.com>
2022-01-10 16:52:21 +0100

    Fix android sample [skip ci]

--- samples/android/app/src/main/java/com/configcat/configcatsample/MainActivity.kt
+++ samples/android/app/src/main/java/com/configcat/configcatsample/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.configcat.configcatsample
 
 import android.annotation.SuppressLint
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.configcat.*
