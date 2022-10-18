bbc034652fdc806365f5aea49ee25a161d42328b
Peter Csajtai <peter.csajtai@outlook.com>
2022-10-18 11:24:05 +0200

    Update ConfigCatClientTest.java

--- src/test/java/com/configcat/ConfigCatClientTest.java
+++ src/test/java/com/configcat/ConfigCatClientTest.java
@@ -6,8 +6,6 @@ import okhttp3.mockwebserver.MockWebServer;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.time.Duration;
-import java.time.Instant;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
